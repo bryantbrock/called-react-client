6da6a4c7b1b3a17d328fdb517cbb5d99b57190b7
Seth Thoburn <seth.thoburn@gmail.com>
2020-07-08 10:21:50 -0400

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter} from "react-router-dom"
 import {PersistGate} from 'redux-persist/integration/react'
 import {Routing} from "app/routing"
 import {store, persistor} from 'store'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 
 class App extends React.Component {
   render() {
@@ -14,6 +16,9 @@ class App extends React.Component {
             <Routing />
           </BrowserRouter>
         </PersistGate>
+        <Container>
+          <h1>App</h1>
+        </Container>
       </Provider>
     )
   }
