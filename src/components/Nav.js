2549ab9117088a1499bec0bb63bcc757bf32f504
Seth Thoburn <seth.thoburn@gmail.com>
2020-07-09 15:40:59 -0400

    add navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
+import BSNav from 'react-bootstrap/Nav'
 
 export class Nav extends Component {
   render() {
     return (
-      <div>
-        THis will be the navigation bar
-      </div>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#">NSA Event Registration</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <BSNav className="mr-auto">
+          <BSNav.Link href="/dashboard">Dashboard</BSNav.Link>
+          <BSNav.Link href="/events">Events</BSNav.Link>
+        </BSNav>
+        <Button onClick={() => this.logout()}>Logout</Button>
+      </Navbar.Collapse>
+    </Navbar>
     )
   }
 }
