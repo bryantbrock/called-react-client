acf289be1cac12a1f798675ea34563cdf2e59429
Seth Thoburn <seth.thoburn@gmail.com>
2020-09-22 11:39:06 -0700

    redirect to https

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 app.use(favicon(__dirname + '/build/favicon.png'))
 
 // the __dirname is the current directory from where the script is running
